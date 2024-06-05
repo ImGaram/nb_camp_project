53182676fa4e23326a3e66c059c88cf6fe20b95b
ImGaram <gim78014@gmail.com>
2024-06-05 21:19:18 +0900

    :sparkles: :: 6월 1주차 개인과제 계산기 틀 구성.

--- calculator_console/src/Main.kt
+++ calculator_console/src/Main.kt
@@ -1,5 +1,6 @@
 package src
 
+import src.calculate.Calculator
 import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)){
@@ -7,27 +8,40 @@ fun main() = with(Scanner(System.`in`)){
     println("계산을 시작할까요?")
     println("계산을 시작하려면 0을 입력하고, 종료하려면 -1을 입력하세요.")
 
+    var isCalculatorFinish = false
     while (true) {
+        // 계산기 종료 여부가 true면 종료
+        if (isCalculatorFinish) break
+
         val mainInput = next()
         // mainInput을 올바르게 작성시키기 위한 try catch
         try {
             if (mainInput.toInt() == 0) {
                 // 계산기 시작.
                 println("계산을 시작합니다.")
-                println("어떤 연산을 하시겠습니까?(1: 덧셈, 2: 뺄셈, 3: 곱셈, 4: 나눗셈, 0: 연산 종료")
                 while (true) {
-                    try {
-                        val calculateValue = next()
-                        if (calculateValue.toInt() == 0) break
-                        calculation(calculateValue = calculateValue.toInt())
-                    } catch (e: Exception) {
-                        println("올바른 문자를 입력해 주쇼..")
+                    println("사칙연산식을 입력하세요(종료하려면 ` 입력).")
+
+                    val formula = next()
+                    if (formula == "`") {
+                        println("계산기를 종료합니다.")
+                        println("=========================계산기 v1=========================")
+                        isCalculatorFinish = true
+                        break
+                    } else {
+                        calculation(formula = formula)
+                        // 정규 표현식을 이용한 수식 분할(여러 개의 문자를 기준으로 분할 가능함).
+                        val splitFormula = formula.split("+", "-", "*", "/")
+                        val splitFormulaSymbol = formula.map { it == '+' || it == '-' || it == '*' || it == '/' }
+                        // splitFormula에서 숫자와 괄호"()"만 포함되도록 하기(정규 표현식 사용).
+                        if (!splitFormula.all { it.matches(Regex("[\\d()]*")) })
+                            println("연산식이 문자를 포함하고 있습니다. 다시 시도해 주세요.")
+                        else {
+//                            calculation()
+                            continue
+                        }
                     }
                 }
-
-                println("계산기를 종료합니다.")
-                println("=========================계산기 v1=========================")
-                break
             } else if (mainInput.toInt() == -1) {
                 // 계산기 종료
                 println("계산기를 종료합니다.")
@@ -45,6 +59,7 @@ fun main() = with(Scanner(System.`in`)){
     }
 }
 
-private fun calculation(calculateValue: Int) {
-    println("연산하기.")
+private fun calculation(formula: String) {
+    // 수식을 입력받아 계산하기.
+    val calculator = Calculator()
 }
