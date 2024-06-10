ea1444bb5930777c2b8e23729a31ed818f8a42eb
ImGaram <gim78014@gmail.com>
2024-06-10 21:44:39 +0900

    :sparkles: :: console calculator Lv.5 구현 해설.

--- calculator_console_answer/src/Main.kt
+++ calculator_console_answer/src/Main.kt
@@ -1,17 +1,22 @@
 package src
 
 import src.calculate.*
+import src.stack.MyStack
 
 fun main() {
     val addCalc = Calculator(AddOperation())
-    println("10 + 20 = ${addCalc.operate(10, 20)} 입니다.")
+    println("10더하기 20 결과는 : ${addCalc.operate(10, 20)} 입니다")
 
     val minusCalc = Calculator(SubstractOperation())
-    println("20 - 10 = ${minusCalc.operate(20, 10)} 입니다.")
+    println("20빼기 10 결과는 : ${minusCalc.operate(20, 10)} 입니다")
 
-    val multiplyCalc = Calculator(MultiplyOperation())
-    println("10 * 20 = ${multiplyCalc.operate(10, 20)} 입니다.")
+    val multipleCalc = Calculator(MultiplyOperation())
+    println("10곱하기 20 결과는 : ${multipleCalc.operate(10, 20)} 입니다")
 
-    val divCalc = Calculator(DivideOperation())
-    println("20 / 10 = ${divCalc.operate(0, 10)} 입니다.")
+    val divideCalc = Calculator(DivideOperation())
+    println("20나누기 10 결과는 : ${divideCalc.operate(20, 10)} 입니다")
+
+    val myStack = MyStack()
+    val calResult = myStack.getPostFixExpressionOperation("(2 + 4) * 4 / 2 * 12")
+    println("결과: ${calResult}입니다")
 }
---
+++ calculator_console_answer/src/stack/MyStack.kt
@@ -0,0 +1,92 @@
+package src.stack
+
+import java.util.Stack
+
+class MyStack {
+    fun getPostFixExpressionOperation(originalExpression: String): Int {
+        val stack = Stack<String>()
+        val arr = strToArr(originalExpression)
+        var result = ""
+
+        for(e in arr) {
+            when(e) {
+                "+", "-", "*", "/" -> {
+                    while(!stack.isEmpty() && precedence(stack.peek()) >= precedence(e)) {
+                        result += stack.pop() + " "
+                    }
+                    stack.push(e)
+                }
+                "(" -> {
+                    stack.push(e)
+                }
+                ")" -> {
+                    while(stack.peek() != "(") {
+                        result += stack.pop() + " "
+                    }
+                    stack.pop() // "(" 제거
+                }
+                else -> {
+                    result += "$e "
+                }
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            result += stack.pop() + " "
+        }
+
+        println("---최종---")
+        println("후위표기법: $result")
+
+        val realResult = finalCalc(result)
+        println("결과: $realResult")
+        return realResult
+    }
+
+    fun finalCalc(result: String): Int {
+        val stack = Stack<String>()
+        val calResult = result.split(" ")
+        var result = 0
+
+        for(e in calResult) {
+            when(e) {
+                "+" -> {
+                    result = stack.pop().toInt() + stack.pop().toInt()
+                    stack.push(result.toString())
+                }
+                "-" -> {
+                    result = -stack.pop().toInt() + stack.pop().toInt()
+                    stack.push(result.toString())
+                }
+                "*" -> {
+                    result = stack.pop().toInt() * stack.pop().toInt()
+                    stack.push(result.toString())
+                }
+                "/" -> {
+                    val num2 = stack.pop().toInt()
+                    val num1 = stack.pop().toInt()
+                    result = num1 / num2
+                    stack.push(result.toString())
+                }
+                else -> {
+                    stack.push(e)
+                }
+            }
+        }
+        return result
+    }
+
+    fun strToArr(str: String): Array<String> {
+        var tempStr = str.replace("(", "( ")
+        tempStr = tempStr.replace(")", " )")
+        return tempStr.split(" ").toTypedArray()
+    }
+
+    fun precedence(operator: String): Int {
+        return when(operator) {
+            "+", "-" -> 1
+            "*", "/" -> 2
+            else -> 0
+        }
+    }
+}
