6b8ae637cc13b647a4ca10278d04f12e3f9927f4
ImGaram <gim78014@gmail.com>
2024-06-14 14:52:16 +0900

    :sparkles: :: console kiosk Lv.3 상속을 위한 부모 클래스 생성.

---
+++ kiosk_console/src/food/Food.kt
@@ -0,0 +1,15 @@
+package src.food
+
+import src.food.category.Category
+
+open class Food(
+    val foodNum: Int,
+    val name: String,
+    val price: Int,
+    val description: String,
+    val category: Category
+) {
+    fun displayInfo() {
+        println("$foodNum. $name                  | W$price | $description")
+    }
+}
