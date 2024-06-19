ba34b85a5ad28be538c4186826fb9024b209353e
ImGaram <gim78014@gmail.com>
2024-06-19 20:32:24 +0900

    :sparkles: :: Android 주특기 입문 개인 과제 Lv.1 회원가입 버튼 클릭 이벤트 생성

--- login_application/app/src/main/java/com/example/loginapplication/SignUpActivity.kt
+++ login_application/app/src/main/java/com/example/loginapplication/SignUpActivity.kt
@@ -1,29 +1,20 @@
 package com.example.loginapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignInActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val loginButton = findViewById<Button>(R.id.button_login)
-        val signUpButton = findViewById<Button>(R.id.button_sign_up)
-
-        signUpButton.setOnClickListener {
-            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
