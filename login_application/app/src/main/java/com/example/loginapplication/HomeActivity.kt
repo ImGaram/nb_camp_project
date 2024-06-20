c2a41b254bc324bb0a22b7725339c58164c26c2e
ImGaram <gim78014@gmail.com>
2024-06-20 14:21:49 +0900

    :lipstick: :: Android 주특기 입문 개인 과제 Lv.3 홈 화면 extra 불러오기, 종료 버튼 이벤트 추가

--- login_application/app/src/main/java/com/example/loginapplication/HomeActivity.kt
+++ login_application/app/src/main/java/com/example/loginapplication/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.loginapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,12 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val extraId = intent.getStringExtra("id")
+        val homeTextIdValue = findViewById<TextView>(R.id.text_home_id_value)
+        if (extraId != null) homeTextIdValue.text = extraId
+
+        val finishButton = findViewById<Button>(R.id.button_home_finish)
+        finishButton.setOnClickListener { finish() }
     }
 }
