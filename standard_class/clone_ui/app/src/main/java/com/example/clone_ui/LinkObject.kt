ac74505582e5c495aaf4aaff48e48e6d47fb688c
ImGaram <gim78014@gmail.com>
2024-07-08 23:28:02 +0900

    :sparkles: :: 수준별 학습반 스탠다드 반 2주차 개인 과제 clone ui 앱 아이콘 별 play store 링크 object 추가.

---
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/LinkObject.kt
@@ -0,0 +1,28 @@
+package com.example.clone_ui
+
+object LinkObject {
+    val rankingLink = listOf(
+        "https://play.google.com/store/apps/details?id=com.devsisters.cba",
+        "https://play.google.com/store/apps/details?id=com.percent.aos.luckydefense",
+        "https://play.google.com/store/apps/details?id=com.ujoygp.cherry",
+        "https://play.google.com/store/apps/details?id=com.BluePotionCompany.EosBlack",
+        "https://play.google.com/store/apps/details?id=com.noriworks.gm",
+        "https://play.google.com/store/apps/details?id=com.pixelkorea.google",
+        "https://play.google.com/store/apps/details?id=com.supercell.squad",
+        "https://play.google.com/store/apps/details?id=com.block.juggle",
+        "https://play.google.com/store/apps/details?id=com.dreamgames.royalmatch",
+        "https://play.google.com/store/apps/details?id=com.noriworks.kg"
+    )
+
+    val sponsorLink = listOf(
+        "https://play.google.com/store/apps/details?id=com.HoYoverse.hkrpgoversea",
+        "https://play.google.com/store/apps/details?id=com.qookkagame.sgzzlb.gp.kr",
+        "https://play.google.com/store/apps/details?id=com.supercell.clashroyale",
+        "https://play.google.com/store/apps/details?id=com.nianticlabs.pokemongo",
+        "https://play.google.com/store/apps/details?id=com.miHoYo.GenshinImpact",
+        "https://play.google.com/store/apps/details?id=com.proximabeta.nikke",
+        "https://play.google.com/store/apps/details?id=com.nexon.nsc.maplem",
+        "https://play.google.com/store/apps/details?id=com.nexon.fmk",
+        "https://play.google.com/store/apps/details?id=com.lilithgames.rok.gpkr",
+    )
+}
