af66f06c3ce20dd7d8d6154800a26e38618f45e9
ImGaram <gim78014@gmail.com>
2024-07-08 23:28:23 +0900

    :sparkles: :: 수준별 학습반 스탠다드 반 2주차 개인 과제 clone ui 인기 차트 앱 클릭 이벤트 추가.

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/MainActivity.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.clone_ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +19,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val searchBar = findViewById<ConstraintLayout>(R.id.main_layout_search_bar)
+        searchBar.setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
+
+        // 리스트 클릭 리스너
+        val rank = listOf(
+            R.id.main_popular_rank_1, R.id.main_popular_rank_2, R.id.main_popular_rank_3,
+            R.id.main_popular_rank_4, R.id.main_popular_rank_5, R.id.main_popular_rank_6,
+            R.id.main_popular_rank_7, R.id.main_popular_rank_8, R.id.main_popular_rank_9,
+            R.id.main_popular_rank_10,
+        )
+        for (i in rank.indices) {
+            val rankLay = findViewById<LinearLayout>(rank[i])
+            rankLay.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(LinkObject.rankingLink[i]))
+                startActivity(intent)
+            }
         }
     }
 }
