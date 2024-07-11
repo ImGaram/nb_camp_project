d65b2eab9b46fcc7df6be7781b0d7c7bc47f3eed
ImGaram <gim78014@gmail.com>
2024-07-11 20:12:59 +0900

    :sparkles: :: 주특기 숙련 개인 과제 상품 정보 activity 생성.

---
+++ apple_market_application/app/src/main/java/com/example/applemarket/GoodsInfoActivity.kt
@@ -0,0 +1,33 @@
+package com.example.applemarket
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.applemarket.data.GoodsData
+import com.example.applemarket.databinding.ActivityGoodsInfoBinding
+
+class GoodsInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGoodsInfoBinding
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityGoodsInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val goods = intent.getParcelableExtra("goods", GoodsData::class.java)
+        Log.d("TAG", "onCreate: $goods")
+    }
+}
