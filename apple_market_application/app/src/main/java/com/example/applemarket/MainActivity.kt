ea285c2fa919972792fc8d4eebbdc05ad04ff807
ImGaram <gim78014@gmail.com>
2024-07-11 19:10:57 +0900

    :sparkles: :: 주특기 숙련 개인 과제 상품 recyclerView 적용

--- apple_market_application/app/src/main/java/com/example/applemarket/MainActivity.kt
+++ apple_market_application/app/src/main/java/com/example/applemarket/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.applemarket.adapter.GoodsAdapter
+import com.example.applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val goodsRecyclerView = binding.mainRecyclerViewGoods
+        goodsRecyclerView.layoutManager = LinearLayoutManager(this)
+        goodsRecyclerView.adapter = GoodsAdapter()
     }
 }
