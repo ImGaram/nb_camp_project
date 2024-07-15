99aaee0482a88106878e2ea6db24b6d38432e0c8
ImGaram <gim78014@gmail.com>
2024-07-15 19:43:01 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v0.2 bottom navigation view 적용

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/MainActivity.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/MainActivity.kt
@@ -1,43 +1,59 @@
 package com.example.clone_ui
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.clone_ui.databinding.ActivityMainBinding
+import com.example.clone_ui.fragment.AppsFragment
+import com.example.clone_ui.fragment.BookFragment
+import com.example.clone_ui.fragment.GameFragment
+import com.example.clone_ui.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val searchBar = findViewById<ConstraintLayout>(R.id.main_layout_search_bar)
-        searchBar.setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java))
+        changeFragment(R.id.menu_game)
+        binding.mainLayoutBottomNavigation.setOnItemSelectedListener { menu ->
+            changeFragment(menu.itemId)
+            true
         }
+    }
 
-        // 리스트 클릭 리스너
-        val rank = listOf(
-            R.id.main_popular_rank_1, R.id.main_popular_rank_2, R.id.main_popular_rank_3,
-            R.id.main_popular_rank_4, R.id.main_popular_rank_5, R.id.main_popular_rank_6,
-            R.id.main_popular_rank_7, R.id.main_popular_rank_8, R.id.main_popular_rank_9,
-            R.id.main_popular_rank_10,
-        )
-        for (i in rank.indices) {
-            val rankLay = findViewById<LinearLayout>(rank[i])
-            rankLay.setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(LinkObject.rankingLink[i]))
-                startActivity(intent)
+    private fun changeFragment(itemId: Int) {
+        when (itemId) {
+            R.id.menu_game -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, GameFragment())
+                    .commit()
+            }
+            R.id.menu_apps -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, AppsFragment())
+                    .commit()
+            }
+            R.id.menu_search -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, SearchFragment())
+                    .commit()
+            }
+            R.id.menu_books -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, BookFragment())
+                    .commit()
             }
         }
     }
