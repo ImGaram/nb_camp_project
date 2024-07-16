6ae754dac00ce504b5e77235f450e24749a3d960
ImGaram <gim78014@gmail.com>
2024-07-16 19:01:35 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v0.2 인기 차트 recyclerView adapter 적용

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/tab/PopularChartFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/tab/PopularChartFragment.kt
@@ -1,20 +1,35 @@
 package com.example.clone_ui.fragment.tab
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.clone_ui.adapter.PopularChartAdapter
+import com.example.clone_ui.data.PopularRankObject
 import com.example.clone_ui.databinding.FragmentPopularChartBinding
 
 class PopularChartFragment : Fragment() {
     private lateinit var binding: FragmentPopularChartBinding
+    private var popularList = PopularRankObject.getPopularRank()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentPopularChartBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val popularChartRecyclerView = binding.popularChartRecyclerView
+        val adapter = PopularChartAdapter(requireContext())
+        adapter.submitList(popularList)
+
+        popularChartRecyclerView.layoutManager = LinearLayoutManager(context)
+        popularChartRecyclerView.adapter = adapter
     }
 }
