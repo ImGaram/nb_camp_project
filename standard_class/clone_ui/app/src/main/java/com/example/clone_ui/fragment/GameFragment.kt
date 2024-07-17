174bbddc1bfeca9dc7eee7ffcb994d76e76469c4
ImGaram <gim78014@gmail.com>
2024-07-17 21:13:47 +0900

    :fire: :: 수준별 학습반 개인 과제 v0.2 ViewPager 적용 해제

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/GameFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/GameFragment.kt
@@ -1,16 +1,25 @@
 package com.example.clone_ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.clone_ui.adapter.GameViewPager2Adapter
+import com.example.clone_ui.R
 import com.example.clone_ui.databinding.FragmentGameBinding
-import com.google.android.material.tabs.TabLayoutMediator
+import com.example.clone_ui.fragment.tab.PopularChartFragment
+import com.example.clone_ui.fragment.tab.RecommendFragment
+import com.example.clone_ui.fragment.tab.SoonFragment
+import com.google.android.material.tabs.TabLayout
 
 class GameFragment : Fragment() {
     private lateinit var binding: FragmentGameBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        changeFragment(RecommendFragment())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,14 +27,32 @@ class GameFragment : Fragment() {
     ): View {
         binding = FragmentGameBinding.inflate(inflater)
 
+        val tabTitle = listOf("추천", "인기 차트", "키즈", "신규", "프리미엄", "카테고리")
         with(binding) {
-            val tabTitle = listOf("추천", "인기 차트", "키즈", "신규", "프리미엄", "카테고리")
-            gameViewPager2.adapter = GameViewPager2Adapter(requireActivity())
-            TabLayoutMediator(gameLayoutCategoryTab, gameViewPager2) { tab, pos ->
-                tab.text = tabTitle[pos]
-            }.attach()
+            repeat(tabTitle.size) {
+                gameLayoutCategoryTab.addTab(gameLayoutCategoryTab.newTab().setText(tabTitle[it]))
+            }
+            gameLayoutCategoryTab.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when (tab?.position) {
+                        0 -> changeFragment(RecommendFragment())
+                        1 -> changeFragment(PopularChartFragment())
+                        else -> changeFragment(SoonFragment())
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
         }
 
         return binding.root
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.game_fragment_container_view, fragment)
+            .commit()
     }
 }
