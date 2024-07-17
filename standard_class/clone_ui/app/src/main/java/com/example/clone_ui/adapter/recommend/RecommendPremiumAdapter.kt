ce5323337b532b0fc369d5547c8b4d9bd5af11c7
ImGaram <gim78014@gmail.com>
2024-07-17 21:11:59 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v0.2 하위 RecyclerView의 Adapter 생성

---
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/adapter/recommend/RecommendPremiumAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.clone_ui.adapter.recommend
+
+import android.content.Context
+import android.content.Intent
+import android.icu.text.DecimalFormat
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clone_ui.R
+import com.example.clone_ui.data.PremiumAppData
+import com.example.clone_ui.databinding.ItemRecommendPremiumAppBinding
+
+class RecommendPremiumAdapter(private val context: Context): ListAdapter<PremiumAppData, RecommendPremiumAdapter.ViewHolder>(
+    object: DiffUtil.ItemCallback<PremiumAppData>() {
+        override fun areItemsTheSame(oldItem: PremiumAppData, newItem: PremiumAppData): Boolean = oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: PremiumAppData, newItem: PremiumAppData): Boolean = oldItem == newItem
+    }
+) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recommend_premium_app, parent, false)
+        return ViewHolder(ItemRecommendPremiumAppBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(private val binding: ItemRecommendPremiumAppBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: PremiumAppData) {
+            with(binding) {
+                val priceFormat = DecimalFormat("#,###₩")
+
+                premiumItemAppIcon.setImageResource(data.appIcon)
+                premiumItemAppTitle.text = data.title
+                premiumItemDescription.text = data.description
+                premiumItemStarRate.text = "${data.starRating}★"
+                premiumItemPrice.text = priceFormat.format(data.price)
+
+                root.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(data.link))
+                    context.startActivity(intent)
+                }
+            }
+        }
+    }
+}
---
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/adapter/recommend/RecommendSponsorAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.clone_ui.adapter.recommend
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clone_ui.R
+import com.example.clone_ui.data.SponsorAppData
+import com.example.clone_ui.databinding.ItemRecommendSponsorAppBinding
+
+class RecommendSponsorAdapter(private val context: Context): ListAdapter<SponsorAppData, RecommendSponsorAdapter.ViewHolder>(
+    object: DiffUtil.ItemCallback<SponsorAppData>() {
+        override fun areItemsTheSame(oldItem: SponsorAppData, newItem: SponsorAppData): Boolean = oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: SponsorAppData, newItem: SponsorAppData): Boolean = oldItem == newItem
+    }
+) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recommend_sponsor_app, parent, false)
+        return ViewHolder(ItemRecommendSponsorAppBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(private val binding: ItemRecommendSponsorAppBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: SponsorAppData) {
+            with(binding) {
+                sponsorItemAppIcon.setImageResource(data.appIcon)
+                sponsorItemAppTitle.text = data.title
+                sponsorItemCategory.text = data.category
+                sponsorItemStarRate.text = "${data.starRating}★"
+
+                root.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(data.link))
+                    context.startActivity(intent)
+                }
+            }
+        }
+    }
+}
