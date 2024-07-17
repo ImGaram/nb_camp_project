2b080bd0229a9f9549caf3d749f656446f6b22db
ImGaram <gim78014@gmail.com>
2024-07-17 21:12:44 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v0.2 추천 화면 RecyclerView 적용

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/tab/RecommendFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/tab/RecommendFragment.kt
@@ -5,16 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.clone_ui.adapter.RecommendAdapter
+import com.example.clone_ui.data.`object`.RecommendObject
 import com.example.clone_ui.databinding.FragmentRecommendBinding
 
 class RecommendFragment : Fragment() {
     private lateinit var binding: FragmentRecommendBinding
 
-     override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentRecommendBinding.inflate(inflater)
+        val adapter = RecommendAdapter(requireContext())
+        val recyclerView = binding.recommendRecyclerView
+
+        adapter.submitList(RecommendObject.getRecommendList())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
         return binding.root
     }
 }
