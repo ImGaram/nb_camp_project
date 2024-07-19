679fde84f94acf628551d60cfb3ef44728152ecc
ImGaram <gim78014@gmail.com>
2024-07-19 19:06:08 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v3 fragment끼리 화면 이동 로직 구현.

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/SearchFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/SearchFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.clone_ui.R
 import com.example.clone_ui.adapter.SearchAdapter
 import com.example.clone_ui.data.`object`.SearchObject
 import com.example.clone_ui.databinding.FragmentSearchBinding
+import com.example.clone_ui.fragment.search.SearchingFragment
 
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
@@ -20,13 +22,21 @@ class SearchFragment : Fragment() {
         binding = FragmentSearchBinding.inflate(inflater)
 
         val recyclerView = binding.searchRecyclerView
-        val adapter = SearchAdapter(requireContext())
+        val adapter = SearchAdapter(
+            requireContext()
+            // todo :: 요기에 클릭 리스너 넣고 안에 replace 넣으면 될듯. bundle도 같이.
+        )
 
         adapter.submitList(SearchObject.getSearchList())
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        binding.searchCardLayout.setOnClickListener {  }
+        binding.searchCardLayout.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SearchingFragment())
+                .addToBackStack(null)
+                .commit()
+        }
 
         return binding.root
     }
