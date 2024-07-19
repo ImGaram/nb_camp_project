f098db0cac8ba7b06fc344a091af25e8e29689bf
ImGaram <gim78014@gmail.com>
2024-07-19 21:40:56 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v3 검색 기록 adapter와 연결

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/search/SearchingFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/search/SearchingFragment.kt
@@ -9,12 +9,17 @@ import android.view.inputmethod.EditorInfo.IME_ACTION_SEARCH
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.clone_ui.R
+import com.example.clone_ui.adapter.SearchingAdapter
+import com.example.clone_ui.data.SearchingData
+import com.example.clone_ui.data.`object`.SearchingHistoryObject
 import com.example.clone_ui.databinding.FragmentSearchingBinding
 import com.example.clone_ui.fragment.SearchFragment
 
 class SearchingFragment : Fragment() {
     private lateinit var binding: FragmentSearchingBinding
+    private lateinit var adapter: SearchingAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +28,16 @@ class SearchingFragment : Fragment() {
         binding = FragmentSearchingBinding.inflate(inflater)
         val res = arguments?.getString("category")
         val editText = binding.searchingEditText
+        val recyclerView = binding.searchingHistoryRecyclerView
 
         editText.setText(res)
         editText.setKeyboard()
+
+        adapter = SearchingAdapter { editText.setText(it) }
+        adapter.submitList(SearchingHistoryObject.getHistory())
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
 
         binding.searchingBackImage.setOnClickListener {
             parentFragmentManager.beginTransaction()
@@ -46,6 +58,8 @@ class SearchingFragment : Fragment() {
 
         setOnEditorActionListener { _, actionId, _ ->
             if (actionId == IME_ACTION_SEARCH) {
+                refreshRecycler(text.toString())
+
                 // 키보드 닫기.
                 val inputMethodManager = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 inputMethodManager.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
@@ -53,5 +67,17 @@ class SearchingFragment : Fragment() {
             }
             true
         }
+    }
+
+    private fun refreshRecycler(search: String) {
+        val historyList = adapter.currentList.toMutableList()
+        historyList.add(
+            SearchingData(
+                id = historyList.size + 1,
+                searchText = search
+            )
+        )
+
+        adapter.submitList(historyList)
     }
 }
