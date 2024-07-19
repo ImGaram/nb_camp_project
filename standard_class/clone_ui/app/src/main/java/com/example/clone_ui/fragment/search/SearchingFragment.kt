fd1cb40cb18a389c2a3aa402b6bd8ee42ef06826
ImGaram <gim78014@gmail.com>
2024-07-19 21:10:04 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v3 searching fragment 검색 바 기능 구현

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/search/SearchingFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/fragment/search/SearchingFragment.kt
@@ -1,60 +1,57 @@
 package com.example.clone_ui.fragment.search
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo.IME_ACTION_SEARCH
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.fragment.app.Fragment
 import com.example.clone_ui.R
+import com.example.clone_ui.databinding.FragmentSearchingBinding
+import com.example.clone_ui.fragment.SearchFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSearchingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_searching, container, false)
+    ): View {
+        binding = FragmentSearchingBinding.inflate(inflater)
+        val res = arguments?.getString("category")
+        val editText = binding.searchingEditText
+
+        editText.setText(res)
+        editText.setKeyboard()
+
+        binding.searchingBackImage.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SearchFragment())
+                .commit()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun EditText.setKeyboard() {
+        requestFocus()
+        postDelayed({
+            // 키보드 열기.
+            val inputMethodManager = activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+        }, 30)
+
+        setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == IME_ACTION_SEARCH) {
+                // 키보드 닫기.
+                val inputMethodManager = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+                clearFocus()
             }
+            true
+        }
     }
 }
