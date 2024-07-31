f300f843bac27596054be55d37027d26350523f6
ImGaram <gim78014@gmail.com>
2024-07-31 19:39:59 +0900

    :sparkles: :: 앱 개발 심화 개인 과제 navHost, bottomNavigationBar 연결

--- image_locker/app/src/main/java/com/example/imagelocker/MainActivity.kt
+++ image_locker/app/src/main/java/com/example/imagelocker/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.rememberNavController
+import com.example.imagelocker.navigation.BottomNavHost
+import com.example.imagelocker.navigation.LockerBottomNavigation
+import com.example.imagelocker.navigation.searchRoute
 import com.example.imagelocker.ui.theme.ImageLockerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
             ImageLockerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        LockerBottomNavigation(
+                            navController = navController,
+                            containerColor = Color.White
+                        )
+                    }
+                ) { innerPadding ->
+                    BottomNavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navHostController = navController,
+                        startDestination = searchRoute
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ImageLockerTheme {
-        Greeting("Android")
     }
 }
