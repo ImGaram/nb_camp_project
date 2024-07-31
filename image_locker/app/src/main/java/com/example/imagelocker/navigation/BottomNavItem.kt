e691c32f861989c602dcb54588d4ffba2ea6de15
ImGaram <gim78014@gmail.com>
2024-07-31 19:38:23 +0900

    :lipstick: :: 앱 개발 심화 개인 과제 bottom navigation item 구성

---
+++ image_locker/app/src/main/java/com/example/imagelocker/navigation/BottomNavItem.kt
@@ -0,0 +1,15 @@
+package com.example.imagelocker.navigation
+
+import com.example.imagelocker.R
+
+const val searchRoute = "route_search"
+const val lockerRoute = "route_locker"
+
+sealed class BottomNavItem(
+    val title: String,
+    val icon: Int,
+    val screenRoute: String
+) {
+    data object Search: BottomNavItem(title = "검색", icon = R.drawable.ic_search, screenRoute = searchRoute)
+    data object Locker: BottomNavItem(title = "저장소", icon = R.drawable.ic_locker, screenRoute = lockerRoute)
+}
