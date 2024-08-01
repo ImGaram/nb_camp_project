e41c2855571a812d8a58235eccd85e14ba0c8a71
ImGaram <gim78014@gmail.com>
2024-08-01 19:25:47 +0900

    :sparkles: :: 앱 개발 심화 개인 과제 response 통합용 data class 생성

---
+++ image_locker/app/src/main/java/com/example/imagelocker/data/response/ResultDocument.kt
@@ -0,0 +1,7 @@
+package com.example.imagelocker.data.response
+
+data class ResultDocument(
+    val title: String,
+    val imageUrl: String,
+    val datetime: String
+)
---
+++ image_locker/app/src/main/java/com/example/imagelocker/data/response/ResultResponse.kt
@@ -0,0 +1,5 @@
+package com.example.imagelocker.data.response
+
+data class ResultResponse(
+    val documents: List<ResultDocument>
+)
