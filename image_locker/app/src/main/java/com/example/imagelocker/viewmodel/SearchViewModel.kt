441d8d9af93f1a4c2c4f73fcb0a4d33444d54cd7
ImGaram <gim78014@gmail.com>
2024-08-02 15:01:56 +0900

    :sparkles: :: 앱 개발 심화 개인 과제 다음 페이지의 정보를 불러오는 함수 생성

--- image_locker/app/src/main/java/com/example/imagelocker/viewmodel/SearchViewModel.kt
+++ image_locker/app/src/main/java/com/example/imagelocker/viewmodel/SearchViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SearchViewModel(private val repository: KakaoSearchRepository = KakaoSearchRepository()): ViewModel() {
-    private val _searchResult = MutableStateFlow<List<ResultDocument>>(emptyList())
+    private val _searchResult = MutableStateFlow<MutableList<ResultDocument>>(mutableListOf())
     val searchResult: StateFlow<List<ResultDocument>> = _searchResult.asStateFlow()
 
     private val _isLoading = MutableStateFlow<Boolean?>(null)
@@ -18,12 +18,28 @@ class SearchViewModel(private val repository: KakaoSearchRepository = KakaoSearc
 
     fun getSearchList(query: String, page: Int) {
         viewModelScope.launch {
+            _searchResult.value.clear()
+
             _isLoading.value = false
             val searchImages = repository.getSearchImages(query, page)
             val searchVideos = repository.getSearchVideos(query, page)
 
             val result = repository.combinationResult(searchImages, searchVideos)
-            _searchResult.value = result.sortedByDescending { it.datetime }
+            _searchResult.value = result.sortedByDescending { it.datetime }.toMutableList()
+
+            if (result.isNotEmpty()) _isLoading.value = true
+        }
+    }
+
+    // 다음 페이지의 리스트가 보여짐.
+    fun getNextPageSearchList(query: String, page: Int) {
+        viewModelScope.launch {
+            _isLoading.value = false
+            val searchImages = repository.getSearchImages(query, page)
+            val searchVideos = repository.getSearchVideos(query, page)
+
+            val result = repository.combinationResult(searchImages, searchVideos)
+            _searchResult.value.addAll(result.sortedByDescending { it.datetime })
 
             if (result.isNotEmpty()) _isLoading.value = true
         }
