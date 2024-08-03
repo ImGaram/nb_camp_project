40d7f1638e4d7e8120ba16246fe46c0133ca30c6
ImGaram <gim78014@gmail.com>
2024-08-03 23:53:44 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v3 인기 차트 viewModel 적용

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/view/fragment/tab/PopularChartFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/view/fragment/tab/PopularChartFragment.kt
@@ -5,31 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.clone_ui.view.adapter.PopularChartAdapter
-import com.example.clone_ui.data.`object`.PopularChartObject
 import com.example.clone_ui.databinding.FragmentPopularChartBinding
+import com.example.clone_ui.viewmodel.PopularChartViewModel
 
 class PopularChartFragment : Fragment() {
     private lateinit var binding: FragmentPopularChartBinding
-    private var popularList = PopularChartObject.getPopularRank()
+    private val popularChartViewModel: PopularChartViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (popularChartViewModel.popularChart.value.isNullOrEmpty()) {
+            popularChartViewModel.getPopularChart()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPopularChartBinding.inflate(inflater)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         val popularChartRecyclerView = binding.popularChartRecyclerView
         val adapter = PopularChartAdapter(requireContext())
-        adapter.submitList(popularList)
+
+        popularChartViewModel.popularChart.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) adapter.submitList(it)
+        }
 
         popularChartRecyclerView.layoutManager = LinearLayoutManager(context)
         popularChartRecyclerView.adapter = adapter
+
+        return binding.root
     }
 }
