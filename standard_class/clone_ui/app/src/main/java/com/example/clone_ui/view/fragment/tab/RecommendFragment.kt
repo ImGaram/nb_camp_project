232dead1c6ba08814cdda9ebc12efcbc47228a49
ImGaram <gim78014@gmail.com>
2024-08-03 23:38:48 +0900

    :sparkles: :: 수준별 학습반 개인 과제 v3 추천 화면 viewModel 불러오는 로직 적용

--- standard_class/clone_ui/app/src/main/java/com/example/clone_ui/view/fragment/tab/RecommendFragment.kt
+++ standard_class/clone_ui/app/src/main/java/com/example/clone_ui/view/fragment/tab/RecommendFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.clone_ui.view.adapter.RecommendAdapter
-import com.example.clone_ui.data.`object`.RecommendObject
 import com.example.clone_ui.databinding.FragmentRecommendBinding
+import com.example.clone_ui.view.adapter.RecommendAdapter
+import com.example.clone_ui.viewmodel.RecommendViewModel
 
 class RecommendFragment : Fragment() {
     private lateinit var binding: FragmentRecommendBinding
+    private val recommendViewModel: RecommendViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (recommendViewModel.recommendList.value.isNullOrEmpty()) {
+            recommendViewModel.getRecommendList()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,9 +31,14 @@ class RecommendFragment : Fragment() {
         val adapter = RecommendAdapter(requireContext())
         val recyclerView = binding.recommendRecyclerView
 
-        adapter.submitList(RecommendObject.getRecommendList())
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
+
+        recommendViewModel.recommendList.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                adapter.submitList(recommendViewModel.recommendList.value)
+            }
+        }
 
         return binding.root
     }
